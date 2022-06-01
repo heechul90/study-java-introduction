4289af81ea001780afdad013d2a4a64a4021ab0e
hclee <heechul4296@gmail.com>
2022-06-01 14:33:39 +0900

    upload 초기소스

---
+++ src/test/java/study/java/introduction/IntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package study.java.introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
